29454de3f1a39285b5632427fab246f169c6e991
frost0807 <junesuck99@gmail.com>
2023-02-12 17:24:41 +0900

    :lipstick:style: 임시 비밀번호 발급 디자인 구현

--- frontend/src/views/user/ForgotPasswordView.vue
+++ frontend/src/views/user/ForgotPasswordView.vue
@@ -3,9 +3,15 @@
 <template>
   <h2>임시 비밀번호 발급</h2>
   <v-form @submit.prevent="sendTempPassword">
-    <v-text-field v-model="userData.email" label="이메일"> </v-text-field>
-    <v-text-field v-model="userData.name" label="이름"> </v-text-field>
-    <v-btn type="submit">메일 발송하기</v-btn>
+    <p class="label">이메일</p>
+    <v-text-field v-model="userData.email" variant="outlined" density="compact">
+    </v-text-field>
+    <p class="label">이름</p>
+    <v-text-field v-model="userData.name" variant="outlined" density="compact">
+    </v-text-field>
+    <v-btn type="submit" height="50" width="100%" variant="outlined"
+      >임시 비밀번호 메일 발송하기</v-btn
+    >
   </v-form>
 </template>
 <script>
@@ -40,3 +46,13 @@ export default {
   }
 }
 </script>
+<style scoped>
+.v-btn {
+  border-radius: 10px;
+}
+.label {
+  text-align: left;
+  font-size: 12px;
+  font-weight: bold;
+}
+</style>
